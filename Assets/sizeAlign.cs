9333ad6c6f41487534e990bd14df3023ad584606
Speedoxx <maximusngjp@gmail.com>
2025-02-15 14:00:38 +0800

    Tilemap

---
+++ Assets/sizeAlign.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sizeAlign : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float height;
+    [SerializeField] float width;
+    [SerializeField] float x_pos;
+    [SerializeField] float y_pos;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<BoxCollider2D>().size = new Vector2(width, height);
+        GetComponent<SpriteRenderer>().size = new Vector2(width, height);
+        GetComponent<Transform>().position = new Vector2(x_pos,y_pos);
+    }
+}
